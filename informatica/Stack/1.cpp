447f0440e512cd6f6ada4377f31a34e4339b6903
AndreMaestro <rybalovandrey2007@gmail.com>
2025-04-08 19:58:52 +0400

    1 задача

--- informatica/Stack/1.cpp
+++ informatica/Stack/1.cpp
@@ -7,29 +7,70 @@
 using namespace std;
 
 struct stack{
-    char inf;
+    string inf;
     stack *next;
 };
 
-void push (stack *&h, char X){
+void push (stack *&h, string X){
     stack *r = new stack;
     r ->inf = X;
     r ->next = h;
     h = r;
 }
 
-char pop(stack *& h) {
-    char X = h -> inf;
+string pop(stack *& h) {
+    string X = h -> inf;
     stack *p = h;
     h = h -> next;
     delete p;
     return X;
 }
 
-void reverse (stack *& h) {
-    stack *head1 = NULL;
-    while(h){
-        push(head1, pop(h));
+stack* filterWordsByLastLetter(stack *& h, char letter){
+    stack* tmpStack = nullptr;
+    stack* resStack = nullptr;
+
+    while(h != nullptr) {
+        string w = pop(h);
+        push(tmpStack, w);
+
+
+        if (w.back() == letter) {
+            push(resStack, w);
+        }
     }
-    h = head1;
+
+    return resStack;
+}
+
+void printStack(stack *& h) {
+    stack* tmp = h;
+    while(tmp != nullptr){
+        cout << tmp -> inf << " ";
+        tmp = tmp -> next;
+    }
+    cout << endl;
+}
+
+int main() {
+    stack* original = nullptr;
+
+    push(original, "aaa");
+    push(original, "fdds");
+    push(original, "sss");
+    push(original, "asd");
+    push(original, "qwe");
+    push(original, "rty");
+    push(original, "fes");
+
+    cout << "Исходный стек: ";
+    printStack(original);
+
+    char targetLetter = 's';
+    stack* result = filterWordsByLastLetter(original, targetLetter);
+
+    cout << " Слова, оканчивающиеся на '" << targetLetter << "': ";
+    printStack(result);
+
+    return 0;
 }
